ef8ea284f7bd3ccccf2480a47559806043e8b8f7
sofy <bedlusofi@gmail.com>
2023-07-06 10:59:31 +0000

    task-1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+define type-annotated function concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """return concatenated string"""
+    return str1 + str2
