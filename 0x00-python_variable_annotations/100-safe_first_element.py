59724fe2f2fab70191b4aace917898067d8fd12b
sofy <bedlusofi@gmail.com>
2023-07-06 15:22:36 +0000

    task-100

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+define a type-annotated function
+"""
+from typing import Sequence, Union, Any
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
