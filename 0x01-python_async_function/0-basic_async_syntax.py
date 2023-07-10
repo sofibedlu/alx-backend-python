ffcf98c1ab7e01b3acd736aac29b82a1ec95ed2c
sofy <bedlusofi@gmail.com>
2023-07-10 16:54:59 +0000

    change argument typed

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -9,7 +9,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay: float = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """
     return random delay value after await
     """
