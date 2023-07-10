7f027c50053210d68404f22cd38950f0373ae989
sofy <bedlusofi@gmail.com>
2023-07-10 17:08:39 +0000

    update-type

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,11 +5,14 @@ define async routine wait_n
 """
 import asyncio
 import queue
+from typing import List
+from asyncio import Task
+
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     it spawn wait_random n times with the specified
     max_delay
@@ -17,14 +20,14 @@ async def wait_n(n: int, max_delay: int) -> float:
 
     delays = queue.PriorityQueue()
 
-    async def handle_task(task):
-        delay = await task
+    async def handle_task(task: Task):
+        delay: float = await task
         delays.put(delay)
 
     tasks = [wait_random(max_delay) for _ in range(n)]
     await asyncio.gather(*(handle_task(task) for task in tasks))
 
-    ordered_delays = []
+    ordered_delays: List[float] = []
     while not delays.empty():
         ordered_delays.append(delays.get())
 
