5a620db9773c0c4e138f85768544a36deacf12dd
sofy <bedlusofi@gmail.com>
2023-07-10 13:27:04 +0000

    task-1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""
+define async routine wait_n
+"""
+import asyncio
+import queue
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: float, max_delay: float) -> float:
+    """
+    it spawn wait_random n times with the specified
+    max_delay
+    """
+
+    delays = queue.PriorityQueue()
+
+    async def handle_task(task):
+        delay = await task
+        delays.put(delay)
+
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    await asyncio.gather(*(handle_task(task) for task in tasks))
+
+    ordered_delays = []
+    while not delays.empty():
+        ordered_delays.append(delays.get())
+
+    return ordered_delays
