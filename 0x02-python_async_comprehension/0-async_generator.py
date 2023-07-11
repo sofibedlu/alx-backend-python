6e5810ae85b0eb38aa1709e626102907eed54fc9
sofy <bedlusofi@gmail.com>
2023-07-11 20:31:22 +0300

    update-style

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,6 +10,9 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
+    """
+    yields random delay with simulated asynchronous op
+    """
     for _ in range(10):
         delay = random.uniform(0, 10)
         await asyncio.sleep(1)
