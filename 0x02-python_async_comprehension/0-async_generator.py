fe422a0eb672b875168433538d3df47cb3d4ca5a
sofy <bedlusofi@gmail.com>
2023-07-11 20:21:45 +0300

    initial-commit

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+define async_generator()
+"""
+
+import random
+import asyncio
+from typing import Generator
+
+async def async_generator() -> Generator[int, None, None]:
+    for _ in range(10):
+        delay = random.uniform(0, 10)
+        await asyncio.sleep(1)
+        yield delay
