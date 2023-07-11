d290b32f7837bbb97eec8748f0710d653804dc5a
sofy <bedlusofi@gmail.com>
2023-07-11 20:28:17 +0300

    add-typed-annoted

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,7 +8,7 @@ import random
 import asyncio
 from typing import Generator
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     for _ in range(10):
         delay = random.uniform(0, 10)
         await asyncio.sleep(1)
