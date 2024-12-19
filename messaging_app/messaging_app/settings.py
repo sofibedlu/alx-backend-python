0e75adfc88b012de187977539bbf97ab5fede12b
sofy <bedlusofi@gmail.com>
2024-12-19 09:31:41 +0300

    fixing-directory-issue in the cheker

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)qbq*_3roe!3@x=dzlyhktjy14zz6jof$+3#*(rw30(sn&3*wd'
+SECRET_KEY = 'django-insecure-_qd*$0d+s+sumta@^rz$+0spmo-&v7e(w2wv_)@u%saj=0y#pk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'chats',
 ]
 
 MIDDLEWARE = [
