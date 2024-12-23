6dddd9cb876a5adf40f3eb5d0b122601a9fc0831
sofy <bedlusofi@gmail.com>
2024-12-23 16:58:47 +0300

    add user, conversation, and message models for chat functionality

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
-# Create your models here.
+class User(AbstractUser):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    phone_numer = models.CharField(max_length=15, null=True)
+    ROLE_CHOICES = [
+        ('guest', 'Guest'),
+        ('host', 'Host'),
+        ('admin', 'Admin'),
+    ]
+    role = models.CharField(max_length=5, choices=ROLE_CHOICES, null=False)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['email']),
+        ]
+
+class Conversation(models.Model):
+    conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    participants = models.ManyToManyField(User, related_name='conversations')
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    sender_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name="messages")
+    message_body = models.TextField(null=False)
+    sent_at = models.DateTimeField(auto_now_add=True, editable=False)
